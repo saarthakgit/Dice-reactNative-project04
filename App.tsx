fa36060c5481820e89ad0ac48d87594198e87839
Sarthak Chauhan <saarthakgid@gmail.com>
2025-08-31 16:41:17 +0530

    Initial Build: pixelated theme, black and white minimal UI and app logo

--- App.tsx
+++ App.tsx
@@ -1,45 +1,90 @@
-/**
- * Sample React Native App
- * https://github.com/facebook/react-native
- *
- * @format
- */
+import { StyleSheet, Text, TouchableOpacity, View , Image} from 'react-native'
+import React , {useState} from 'react'
+import ReactNativeHapticFeedback from "react-native-haptic-feedback";
 
-import { NewAppScreen } from '@react-native/new-app-screen';
-import { StatusBar, StyleSheet, useColorScheme, View } from 'react-native';
-import {
-  SafeAreaProvider,
-  useSafeAreaInsets,
-} from 'react-native-safe-area-context';
+const options = {
+  enableVibrateFallback: true,
+  ignoreAndroidSystemSettings: false,
+};
 
-function App() {
-  const isDarkMode = useColorScheme() === 'dark';
+const App = () => {
+  const [isPressed , setIsPressed] = useState(false)
+  const[isLoggedin , setIsLoggedIn] = useState(0)
+ // const vibration = ()=> ReactNativeHapticFeedback.trigger("impactLight");
+  const shuffle : Function = ()=>{
 
-  return (
-    <SafeAreaProvider>
-      <StatusBar barStyle={isDarkMode ? 'light-content' : 'dark-content'} />
-      <AppContent />
-    </SafeAreaProvider>
-  );
-}
-
-function AppContent() {
-  const safeAreaInsets = useSafeAreaInsets();
+    const images : Record<any,any> = {
+      '1' : require('./assets/images/1.png'),
+     '2' : require('./assets/images/2.png'),
+     '3' : require('./assets/images/3.png'),
+     '4' : require('./assets/images/4.png'),
+      '5' :require('./assets/images/5.png'),
+     '6' : require('./assets/images/6.png'),
+      
+    }
+    let index  = (Math.floor(Math.random()*6 + 1))
+    let element = (images[index.toString()])
+    const defaultImg = (images['1'])
+    switch (index) {
+      case 0:
+        return element
+        
+      case 1:
+        return element
+      case 2:
+        return element
+      case 3:
+        return element
+      case 4:
+        return element
+      case 5:
+        return element
+       
+    
+      default:
+        return defaultImg
+       
+    }
+    
+  }
 
   return (
     <View style={styles.container}>
-      <NewAppScreen
-        templateFileName="App.tsx"
-        safeAreaInsets={safeAreaInsets}
-      />
+      {isLoggedin == 0 ? (
+        <TouchableOpacity onPress={()=>{ ReactNativeHapticFeedback.trigger("impactLight",options);setIsLoggedIn(1);}}>
+        <Image style={styles.dice} source={require('./assets/images/1.png')}/>
+      </TouchableOpacity>
+
+      ) : !isPressed && isLoggedin == 1 ? (
+                <TouchableOpacity onPress={()=>{ ReactNativeHapticFeedback.trigger("impactLight",options);setIsPressed(true);setIsLoggedIn(1)}}>
+          <Image style={styles.dice} source={shuffle()}/>
+        </TouchableOpacity>
+      ) : isPressed && isLoggedin == 1 ? (
+                <TouchableOpacity onPress={()=>{ ReactNativeHapticFeedback.trigger("impactLight",options);setIsPressed(false);setIsLoggedIn(1)}}>
+          <Image style={styles.dice} source={shuffle()}/>
+        </TouchableOpacity>) : null
+
+      
+      }
+<Text style = {{fontSize : 30 , marginTop: 40, fontFamily : 'PixelifySans-Bold'}}>Tap to Roll!</Text>
+
     </View>
-  );
+  )
 }
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-  },
-});
+export default App
 
-export default App;
+const styles = StyleSheet.create({
+  container : {
+    backgroundColor : '#FFFFFF',
+    flex : 1,
+    alignItems : 'center',
+    justifyContent : 'center'
+  },
+  dice : {
+    width : 180,
+    height : 180,
+
+  },
+
+})
--- android/app/src/main/java/com/dice/MainApplication.kt
+++ android/app/src/main/java/com/dice/MainApplication.kt
@@ -1,5 +1,6 @@
 package com.dice
 
+import com.mkuczera.RNReactNativeHapticFeedbackPackage;
 import android.app.Application
 import com.facebook.react.PackageList
 import com.facebook.react.ReactApplication
